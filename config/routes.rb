1e03324ef2d32f042e7f0d7328a40045baef7dfe
David Rivers <dmr@DMR-MBP.local>
2014-10-13 14:56:57 -0600

    4th_major_ About and Task links added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   root "pages#index"
-  
+
+  get "/about" => "pages#info_pg", as: :abouty
+  get "/terms" => "pages#cond_pg", as: :termy
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
