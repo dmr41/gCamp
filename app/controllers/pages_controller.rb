57f37a81a8f4f4eb7986ce55d1289af21c2e39b0
David Rivers <dmr@DMR-MBP.local>
2014-10-15 14:17:05 -0600

    attribute variables introduced

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,21 @@ class PagesController < ApplicationController
 
   def index
 
-    @people_quotes = [["Confucius","\"When it is obvious that the goals cannot be reached, don't adjust the goals, adjust the action steps.\""],
-               ["Friedrich Nietzsche","\"The doer alone learneth.\""],
-              ["Albert Camus","\"Autumn is a second spring when every leaf is a flower.\""]]
+
+    quote1 = Quote.new
+    quote1.phrase = "\"When it is obvious that the goals cannot be reached, don't adjust the goals, adjust the action steps.\""
+    quote1.author = "Confucius"
+
+    quote2 = Quote.new
+    quote2.phrase = "\"The doer alone learneth.\""
+    quote2.author = "Friedrich Nietzsche"
+
+    quote3 = Quote.new
+    quote3.phrase = "\"Autumn is a second spring when every leaf is a flower.\""
+    quote3.author = "Albert Camus"
+
+    @people_quotes = [quote1, quote2, quote3]
+
   end
 
 end
---
+++ app/models/quote.rb
@@ -0,0 +1,6 @@
+class Quote
+
+  attr_accessor :author, :phrase
+
+
+end
