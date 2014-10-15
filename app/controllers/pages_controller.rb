8385cb2e82d159473b16dbf4b64f72ad3664ff77
David Rivers <dmr@DMR-MBP.local>
2014-10-15 13:41:23 -0600

    set array variable people_quotes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,10 @@
 class PagesController < ApplicationController
 
+  def index
+
+    @people_quotes = [["Confucius","\"When it is obvious that the goals cannot be reached, don't adjust the goals, adjust the action steps.\""],
+               ["Friedrich Nietzsche","\"The doer alone learneth.\""],
+              ["Albert Camus","\"Autumn is a second spring when every leaf is a flower.\""]]
+  end
+
 end
