09c20e7647154c76d2d507686c02c793313cb5ab
David Rivers <dmr@DMR-MBP.local>
2014-10-16 15:00:31 -0600

    Tasks added

---
+++ app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :description, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :tasks
+
   root "pages#index"
 
   get "/about" => "pages#info_pg", as: :abouty
