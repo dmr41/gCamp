f69dc7399be9a8ba1cbf56491cf24e41b1371cb3
David Rivers <dmr@DMR-MBP.local>
2014-10-22 08:49:50 -0600

    task formatting with date better orgainzed

---
+++ db/migrate/20141022143408_change_default_task_state_to_false.rb
@@ -0,0 +1,5 @@
+class ChangeDefaultTaskStateToFalse < ActiveRecord::Migration
+  def change
+    change_column :tasks, :complete, :boolean, :default=>false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021140823) do
+ActiveRecord::Schema.define(version: 20141022143408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20141021140823) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "complete"
+    t.boolean  "complete",    default: false
     t.date     "date"
   end
 
