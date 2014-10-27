dd2d9aab3b921d23aeae7a0bb0aabab32e099dd5
David Rivers <dmr41.rivers@gmail.com>
2014-10-27 17:35:39 -0600

    added edit button to users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def index
     @users = User.all
   end
@@ -22,12 +26,25 @@ class UsersController < ApplicationController
     end
   end
 
-def user_params
-  params.require(:user).permit(:ufname, :ulname, :uemail)
-end
-
-  def set_user
-    @user = User.find(params[:id])
+  def edit
   end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:ufname, :ulname, :uemail)
+  end
+
 
 end
