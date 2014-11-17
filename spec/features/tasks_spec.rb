fa17f4a47c78a98f9534024a5d9656a7d11c3cdd
David Rivers <dmr41.rivers@gmail.com>
2014-11-17 15:51:17 -0700

    rspec on tasks corrected after nesting of tasks in project

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -4,7 +4,8 @@ require 'spec_helper'
 feature "signup" do
 
   scenario "checks tasks for task creation show and edit" do
-    visit tasks_path
+    project = Project.create!(name: "Billy")
+    visit project_tasks_path(project)
     expect(page).to have_no_content("David")
     expect(page).to have_no_content("12/12/2020")
     click_on "Create Task"
@@ -14,12 +15,13 @@ feature "signup" do
     expect(page).to have_content("David")
     expect(page).to have_content("12/12/2020")
     expect(page).to have_content("Task was successfully created.")
-    click_on "Tasks"
+    save_and_open_page
+    click_on "Back"
     click_on "Show"
     expect(page).to have_content("David")
     expect(page).to have_content("12/12/2020")
     expect(page).to have_no_content("Task was successfully created.")
-    click_on "Tasks"
+    click_on "Back"
     click_on "Edit"
     fill_in "Description", with: "Billy"
     fill_in "Date", with: "11/11/2020"
@@ -33,12 +35,15 @@ feature "signup" do
   end
 
   scenario "checks tasks for task destroy" do
-    visit tasks_path
+    project = Project.create!(name: "Billy")
+    visit project_tasks_path(project)
+    expect(page).to have_no_content("David")
+    expect(page).to have_no_content("12/12/2020")
     click_on "Create Task"
     fill_in "Description", with: "David"
     fill_in "Date", with: "11/11/2020"
     click_on "Create Task"
-    visit tasks_path
+    visit project_tasks_path(project)
     expect(page).to have_content("David")
     expect(page).to have_content("11/11/2020")
     click_on "Destroy"
@@ -47,18 +52,19 @@ feature "signup" do
   end
 
   scenario "Click all vs incomplete" do
-    visit tasks_path
+    project = Project.create!(name: "Billy")
+    visit project_tasks_path(project)
     expect(page).to have_no_content("David")
-    expect(page).to have_no_content("11/11/2020")
+    expect(page).to have_no_content("12/12/2020")
     click_on "Create Task"
     fill_in "Description", with: "David"
     fill_in "Date", with: "11/11/2020"
     click_on "Create Task"
-    visit tasks_path
+    visit project_tasks_path(project)
     click_on "Edit"
     check('Complete')
     click_on "Update Task"
-    visit tasks_path
+    visit project_tasks_path(project)
     expect(page).to have_no_content("David")
     expect(page).to have_no_content("11/11/2020")
     expect(page).to have_no_content("billy")
@@ -67,7 +73,7 @@ feature "signup" do
     fill_in "Description", with: "billy"
     fill_in "Date", with: "12/12/2020"
     click_on "Create Task"
-    visit tasks_path
+    visit project_tasks_path(project)
     expect(page).to have_no_content("David")
     expect(page).to have_content("12/12/2020")
     expect(page).to have_content("billy")
@@ -96,22 +102,30 @@ end
 feature "tasks validation" do
 
   scenario "tasks description validation can't be blank" do
-    visit new_task_path
+    project = Project.create!(name: "Billy")
+    visit project_tasks_path(project)
+    expect(page).to have_no_content("David")
+    expect(page).to have_no_content("12/12/2020")
+    click_on "Create Task"
     click_on "Create Task"
     expect(page).to have_content("Description can't be blank")
   end
 
   include ActiveSupport::Testing::TimeHelpers
-  
+
   scenario "task date select is not before today's date" do
-    visit new_task_path
+    project = Project.create!(name: "Billy")
+    visit project_tasks_path(project)
+    expect(page).to have_no_content("David")
+    expect(page).to have_no_content("12/12/2020")
+    click_on "Create Task"
     travel_to(1.day.ago) do
       fill_in "Date", with: Date.today
     end
     click_on "Create Task"
     expect(page).to have_content("Date is not included in the list")
     expect(page).to have_content("Description can't be blank")
-    visit new_task_path
+    visit new_project_task_path(project)
     fill_in "Description", with: "bobos"
     fill_in "Date", with: Date.today
     click_on "Create Task"
