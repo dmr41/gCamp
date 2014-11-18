1e327493ea6e785231a6f831a7666a35b7ff7031
David Rivers <dmr41.rivers@gmail.com>
2014-11-18 00:17:27 -0700

    Tasks User and Project error messages better displayed in New creations fields.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,7 @@
 class Task < ActiveRecord::Base
 
   validates :description, presence: true
-  validates_inclusion_of  :date, {:on => :create, :in =>  (Date.today)..(Date.today + 20.years)}
-
+  validates_inclusion_of  :date, {:on => :create, :in =>  (Date.today)..(Date.today + 20.years), :allow_nil => true}
   belongs_to :project
-  
+
 end
