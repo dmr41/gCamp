0fa32ec7e796f6bb7384e577823f9281ce8c68f3
David Rivers <dmr41.rivers@gmail.com>
2014-11-19 11:52:01 -0700

    put validation of user_id uniquely in the scope of project_id - membership model

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,6 +1,6 @@
 class Membership < ActiveRecord::Base
 
-  validates  :user_id, presence: true, uniqueness: true
+  validates  :user_id, presence: true, uniqueness: { scope: :project_id}
   belongs_to :user
   belongs_to :project
 
