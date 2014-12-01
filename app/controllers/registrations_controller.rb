57d5083708a21b076788bbf300019107c1b58148
David Rivers <dmr41.rivers@gmail.com>
2014-12-01 15:09:33 -0700

    new user signup redirects to new projects page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ class RegistrationsController < MarketingController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to new_project_path
     else
       render :new
     end
