53f5253348ad6491b4020cfb054cdcd3e1814d87
David Rivers <dmr41.rivers@gmail.com>
2014-12-01 15:06:44 -0700

    Signed in pre-existing users are redirected to projects index page.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,10 +1,10 @@
 class AuthenticationController < MarketingController
-  
+
   def create
     user = User.find_by_email(params[:authentication][:email].downcase)
     if user && user.authenticate(params[:authentication][:password])
       session[:user_id] = user.id
-      redirect_to root_path
+      redirect_to projects_path
     else
       @sign_in_error = "Username / password combination is invalid"
       render :new
