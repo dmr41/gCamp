3a1b81b62d0d0e6acaace719c4a077704d225fcd
David Rivers <dmr41.rivers@gmail.com>
2014-12-01 15:40:43 -0700

    Projects index page only show projects for users associated with it.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,9 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all
+    if current_user
+      @projects = current_user.projects
+    end
   end
 
   def new
@@ -17,7 +19,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
     if @project.save
       @membership = @project.memberships.create(role: "Owner", user_id: current_user.id, project_id: @project.id)
-      redirect_to @project, notice: "Project Created!"
+      redirect_to project_tasks_path(@project), notice: "Project Created!"
     else
       render :new
     end
