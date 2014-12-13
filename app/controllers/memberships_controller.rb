ed64b8bb5508085569adcdc8dba3cce2d5e92488
David Rivers <dmr41.rivers@gmail.com>
2014-12-12 18:15:30 -0700

    first pass refactor of membership controller destroy method.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -53,44 +53,84 @@ class MembershipsController < ApplicationController
     end
   end
 
+  # def destroy
+  #   @membership = @project.memberships.find(params[:id])
+  #   temp_name = @membership.user.full_name
+  #   if current_user
+  #     if current_user.admin
+  #       @membership.destroy
+  #       redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
+  #     elsif @role == 'Member' &&  @membership.user.id == current_user.id
+  #       @membership.destroy
+  #       redirect_to projects_path, notice: "#{temp_name} was removed successfully."
+  #     elsif @role == 'Owner' && @owner_count > 1
+  #       if @membership.user.id != current_user.id
+  #         @membership.destroy
+  #         redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
+  #       else
+  #         @membership.destroy
+  #         redirect_to projects_path
+  #       end
+  #     elsif @role == 'Owner'
+  #       if @membership.user.id == current_user.id && @owner_count != 1
+  #          @membership.destroy
+  #          redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
+  #       elsif @membership.user.id != current_user.id && @owner_count == 1
+  #          @membership.destroy
+  #          redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
+  #       else
+  #          redirect_to project_memberships_path(@project), notice: "#{temp_name} is the only owner of the project and can't be removed."
+  #       end
+  #     end
+  #    else
+  #      render file: 'public/404.html', status: :not_found, layout: false
+  #    end
+  # end
+
   def destroy
     @membership = @project.memberships.find(params[:id])
-    temp_name = @membership.user.full_name
+    @temp_name = @membership.user.full_name
     if current_user
-      if current_user.admin
+      if single_owner_or_membership || multiple_owner
         @membership.destroy
-        redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
-      elsif @role == 'Member' &&  @membership.user.id == current_user.id
+        redirect_to project_memberships_path(@project), notice: "#{@temp_name} was removed successfully."
+      elsif self_destroy
         @membership.destroy
-        redirect_to projects_path, notice: "#{temp_name} was removed successfully."
-      elsif @role == 'Owner' && @owner_count > 1
-        if @membership.user.id != current_user.id
-          @membership.destroy
-          redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
-        else
-          @membership.destroy
-          redirect_to projects_path
-        end
-      elsif @role == 'Owner'
-        if @membership.user.id == current_user.id && @owner_count != 1
-           @membership.destroy
-           redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
-         elsif @membership.user.id != current_user.id && @owner_count == 1
-           @membership.destroy
-           redirect_to project_memberships_path(@project), notice: "#{temp_name} was removed successfully."
-         else
-           redirect_to project_memberships_path(@project), notice: "#{temp_name} is the only owner of the project and can't be removed."
-         end
-        end
-     else
-       render file: 'public/404.html', status: :not_found, layout: false
-     end
+        redirect_to projects_path, notice: "#{@temp_name} was removed successfully."
+      else
+        redirect_to project_memberships_path(@project), notice: "#{@temp_name} is the only owner of the project and can't be removed."
+      end
     end
+  end
 
   private
     def membership_params
       params.require(:membership).permit(:role, :user_id, :project_id)
     end
 
+  def multiple_owner
+    if current_user.admin
+      return true
+    elsif @role == 'Owner' && @owner_count > 1 && @membership.user.id != current_user.id
+      return true
+    else
+    end
+  end
+
+  def self_destroy
+    if @role == 'Owner' && @owner_count > 1 && @membership.user.id == current_user.id
+      return true
+    elsif @role == 'Member' && @membership.user.id == current_user.id
+      return true
+    else
+    end
+  end
+
+  def single_owner_or_membership
+    if  @role == 'Owner' && @membership.user.id == current_user.id && @owner_count != 1
+      return true
+    else
+    end
+  end
 
 end
