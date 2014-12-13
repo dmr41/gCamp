06f8a8a5d0bd9f53a95e7271c13e8df520149646
David Rivers <dmr41.rivers@gmail.com>
2014-12-13 09:07:29 -0700

    more spec adjusting to prevent faker data collisions

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe ApplicationController do
 
+  before do
+    cleanup_databases
+  end
+  
   describe "#require_login" do
     #
     # it "a user is not required to log in got " do
