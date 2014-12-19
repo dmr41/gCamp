da31a3ff872b03f54e23660e2b5f4c50e76ebb3f
David Rivers <dmr41.rivers@gmail.com>
2014-12-19 05:07:05 -0700

    layout changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def page_header(title, &block)
+    html = '<div class="page-header">'.html_safe
+    if block
+      html += '<div class="pull-right">'.html_safe
+      html += capture do
+                block.call
+              end
+      html += '</div>'.html_safe
+    end
+    html += '<h1>'.html_safe
+    html += title
+    html += '</h1>'.html_safe
+  end
+
 end
